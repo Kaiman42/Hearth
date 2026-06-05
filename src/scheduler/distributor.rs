6b7b410d5e6647d5602cfab2d871d55013584b1a
kaiman <kaimxn42@gmail.com>
2026-06-04 21:30:40 -0300

    update scheduler messaging layer to use NATS

--- src/scheduler/distributor.rs
+++ src/scheduler/distributor.rs
@@ -4,17 +4,14 @@ use anyhow::{bail, Result};
 use hearth_interconnect::messages::{JobRequest, Message};
 use hearth_interconnect::worker_communication::Job;
 use nanoid::nanoid;
-use rdkafka::producer::FutureProducer;
 use std::sync::OnceLock;
 use tokio::sync::Mutex;
-// Handles distribution across worker nodes via round robin or maybe another method?
 
 pub static ROUND_ROBIN_INDEX: OnceLock<Mutex<usize>> = OnceLock::new();
 pub static WORKERS: OnceLock<Mutex<Vec<String>>> = OnceLock::new();
 
 pub async fn distribute_job(
     job: JobRequest,
-    producer: &mut FutureProducer,
     config: &Config,
 ) -> Result<()> {
     let mut index_guard = ROUND_ROBIN_INDEX.get().unwrap().lock().await;
@@ -31,12 +28,7 @@ pub async fn distribute_job(
         request_id: job.request_id,
         guild_id: job.guild_id,
     });
-    send_message(
-        internal_message,
-        config.kafka.kafka_topic.as_str(),
-        producer,
-    )
-    .await;
+    send_message(internal_message).await;
     *index_guard += 1;
     if *index_guard == workers_guard.len() {
         *index_guard = 0;
--- src/worker/connector.rs
+++ src/worker/connector.rs
@@ -1,6 +1,5 @@
 use crate::config::Config;
-use crate::utils::generic_connector::{initialize_producer, send_message_generic};
-use crate::utils::initialize_consume_generic;
+use crate::utils::generic_connector::{nats_connect, nats_listen, nats_publish};
 use crate::worker::errors::report_error;
 use crate::worker::queue_processor::{
     process_job, JobID, ProcessorIPC, ProcessorIPCData, ProcessorIncomingAction,
@@ -10,13 +9,13 @@ use anyhow::Result;
 use hearth_interconnect::messages::{Message, PingPongResponse};
 use hearth_interconnect::worker_communication::Job;
 use log::{debug, error, info};
-use rdkafka::producer::FutureProducer;
+use nats::asynk::Connection;
 use songbird::Songbird;
 use std::sync::{Arc, OnceLock};
 use tokio::sync::broadcast::{Receiver, Sender};
 use tokio::sync::{broadcast, Mutex};
 
-pub static WORKER_PRODUCER: OnceLock<Mutex<Option<FutureProducer>>> = OnceLock::new();
+pub static WORKER_NC: OnceLock<Mutex<Option<Connection>>> = OnceLock::new();
 
 pub async fn queue_internal_job(job: Job, config: &Config, songbird: Option<Arc<Songbird>>) {
     if &job.worker_id == config.config.worker_id.as_ref().unwrap() {
@@ -30,7 +29,6 @@ pub async fn queue_internal_job(job: Job, config: &Config, songbird: Option<Arc<
         let tx_arc = Arc::new(tx_processor);
 
         {
-            // Scoped to minimize lock time
             WORKER_GUILD_IDS
                 .get()
                 .unwrap()
@@ -55,12 +53,18 @@ pub async fn initialize_api(
     config: &Config,
     ipc: &mut ProcessorIPC,
     songbird: Option<Arc<Songbird>>,
-    group_id: &String,
 ) {
-    let broker = config.kafka.kafka_uri.to_string();
-    let _x = config.clone();
-
-    initialize_worker_consume(broker, config, ipc, songbird, group_id).await;
+    let nc = nats_connect(config).await;
+    let _ = WORKER_NC.set(Mutex::new(Some(nc.clone())));
+    nats_listen(
+        &nc,
+        config,
+        parse_message_callback,
+        ipc,
+        initialized_callback,
+        songbird,
+    )
+    .await;
 }
 
 async fn parse_message_callback(
@@ -104,13 +108,6 @@ async fn parse_message_callback(
                     }
                     None => {
                         error!("Failed to route DWC job message - Could not find channel!");
-
-                        // This is a bit sketchy in the future we should probably shift to a better state sync system
-                        // That runs if this happens to resync client and server state across the distributed system
-                        // The main issue with this is that it will only really work with `join` commands
-                        // As all other commands relly on a pre-existing state notion that will be non-existent in this case
-
-                        // If job does not exist create it
                         queue_internal_job(
                             Job {
                                 job_id: job_id.clone(),
@@ -122,26 +119,19 @@ async fn parse_message_callback(
                             songbird,
                         )
                         .await;
-
-                        // We could attempt to perform the action again but this would involve waiting for confirmation
-                        // And would not work most of the time anyway because of a desynced state notion instead we will
-                        // Just create the job and hope the user retry's and it works. This is well... pretty bad
-                        // In the future as a slight upgrade to this we can send an erorr report and have the client auto-retry
-                        // If it was a `join` command otherwise throw an error
                     }
                 }
             }
         }
         Message::InternalPingPongRequest => {
-            let mut px = WORKER_PRODUCER.get().unwrap().lock().await;
-            let p = px.as_mut();
+            let mut nc = WORKER_NC.get().unwrap().lock().await;
+            let nc = nc.as_mut().unwrap();
 
-            send_message(
+            nats_publish(
+                nc,
                 &Message::InternalPongResponse(PingPongResponse {
                     worker_id: config.config.worker_id.clone().unwrap(),
                 }),
-                &config.kafka.kafka_topic,
-                &mut *p.unwrap(),
             )
             .await;
         }
@@ -153,29 +143,10 @@ async fn parse_message_callback(
     Ok(())
 }
 
-pub async fn initialize_worker_consume(
-    brokers: String,
-    config: &Config,
-    ipc: &mut ProcessorIPC,
-    songbird: Option<Arc<Songbird>>,
-    group_id: &String,
-) {
-    let producer: FutureProducer = initialize_producer(&brokers, config);
-    let _ = WORKER_PRODUCER.set(Mutex::new(Some(producer)));
-    initialize_consume_generic(
-        &brokers,
-        config,
-        parse_message_callback,
-        ipc,
-        initialized_callback,
-        songbird,
-        group_id,
-    )
-    .await;
-}
-
 async fn initialized_callback(_: Config) {}
 
-pub async fn send_message(message: &Message, topic: &str, producer: &mut FutureProducer) {
-    send_message_generic(message, topic, producer).await;
+pub async fn send_message(message: &Message) {
+    let mut nc = WORKER_NC.get().unwrap().lock().await;
+    let nc = nc.as_mut().unwrap();
+    nats_publish(nc, message).await;
 }
